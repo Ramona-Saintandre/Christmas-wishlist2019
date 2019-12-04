6225942db9baa15155c6666f0bae6725c760b066
Ramona Saintandre <codinggrandma@gmail.com>
2019-12-04 07:04:48 -0500

    added dom elements

---
+++ main.js
@@ -0,0 +1,4 @@
+const clear = document.querySelector(".clear-item");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
