e74720337946fe0e2255d82521582932dcfdd8d4
Ramona Saintandre <codinggrandma@gmail.com>
2019-12-13 22:21:06 -0500

    starting working on todo array, if statement

--- main.js
+++ main.js
@@ -1,44 +1,62 @@
- 
-// being able to write text to create the list 
-const list = document.getElementById(list);
+ // being able to write text to create the list 
+ const list = document.getElementById(list);
 
-function wishList (addWish){
-const text = `<li class="item">
+ function wishList(addWish) {
+     const text = `<li class="item">
 <i class="fa fa-circle-thin" job ="complete"></i>
 <p class="text"> ${addWish} </p>
 <i class="fa fa-trash-o delete" job="delete"></i>
 </li>`
-const position = "beforeend"; //this will put the wish after the last child 
-// insertAdjacentHTML() need to research
-list.insertAdjacentHTML(position,text);
-}
+     const position = "beforeend"; //this will put the wish after the last child 
+     // insertAdjacentHTML() need to research
+     list.insertAdjacentHTML(position, text);
+ }
 
-const input =document.getElementById('input');
-document.addEventListener('keyup',function(event){
-    if(event.keyCode == 13){
-        const toDo = input.value; 
-        wishList(toDo);
-    }
-    input.value="";
-});
+ const input = document.getElementById('input');
+ document.addEventListener('keyup', function (event) {
+             if (event.keyCode == 13) {
+                 const toDo = input.value;
+                 if (toDo) {
+                     wishList(toDo, id, false, false);
+                     list.push({
+                         name: toDo,
+                         id: id,
+                         done: false,
+                         trash: false,
+                     });
 
-let List = [];
-let id = 0;
+                     input.value = "";
+                     id++;
+                    }
+                }
+                 });
 
-list =[{},{},...];
-list[0];
-{
-    name:"'Drink Coffe",
-    id :0,
-    done : false,
-    trash : false
-}
+             let List = [];
+             let id = 0;
 
-wishList('weighted Blanket');
-console.log("is this working");
+             list = [{}, {}, ...];
+             list[0];
+
+             {
+                 name: "Drink Coffe",
+                 id: 0,
+                 done: false,
+                 trash: false,
+             }
+
+             list[1]; {
+                 name: 'money',
+                 id: 1,
+                 done: true,
+                 trash: false,
+             }
 
 
-// const input = document.getElementById("input");
-// const dateItem = document.getElementById("date");
-// const clear = document.querySelector(".clear-item");
-// element.insertAdjacentHTML(position, text);
+             // wishList('weighted Blanket');
+             // console.log("is this working");
+
+
+             // const input = document.getElementById("input");
+             // const dateItem = document.getElementById("date");
+             // const clear = document.querySelector(".clear-item");
+             // element.insertAdjacentHTML(position, text);
